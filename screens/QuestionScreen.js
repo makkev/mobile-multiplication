bb92d2d2b31f444463e4c74326bb04556536cd14
K Mak <kumarajiva08@gmail.com>
2019-03-23 09:08:10 +1100

    add number pad on question screen

--- screens/QuestionScreen.js
+++ screens/QuestionScreen.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import {
   StyleSheet,
   View,
@@ -8,6 +9,7 @@ import {
   Button,
   Image,
   ImageBackground } from 'react-native';
+
 
 class Question extends Component {
   constructor(props){
@@ -27,6 +29,10 @@ class Question extends Component {
     };
   }
 
+  handlePressNumber = () => {
+
+  }
+
   render() {
     const { questions, tablesArray } = this.props.navigation.state.params;
     const { score, answer, time, currentQuestionIndex } = this.state;
@@ -44,8 +50,101 @@ class Question extends Component {
         */}
         <View style={styles.topSection}>
           <Text style={styles.topText}>
-          {`${question.number1} x ${question.number2} = `}
+          {`${question.number1}  x  ${question.number2}  =  `}
           </Text>
+        </View>
+        <View style={styles.bottomSection}> 
+          <View style={styles.buttonSection}>
+            <TouchableOpacity
+              onPress={() => this.handlePressNumber()}
+              style={styles.numberButton}
+            >
+              <Text style={styles.tablesBtnText}>1</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => this.handlePressNumber()}
+              style={styles.numberButton}
+            >
+              <Text style={styles.tablesBtnText}>2</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => this.handlePressNumber()}
+              style={styles.numberButton}
+            >
+              <Text style={styles.tablesBtnText}>3</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.buttonSection}>
+            <TouchableOpacity
+              onPress={() => this.handlePressNumber()}
+              style={styles.numberButton}
+            >
+              <Text style={styles.tablesBtnText}>4</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => this.handlePressNumber()}
+              style={styles.numberButton}
+            >
+              <Text style={styles.tablesBtnText}>5</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => this.handlePressNumber()}
+              style={styles.numberButton}
+            >
+              <Text style={styles.tablesBtnText}>6</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.buttonSection}>
+            <TouchableOpacity
+              onPress={() => this.handlePressNumber()}
+              style={styles.numberButton}
+            >
+              <Text style={styles.tablesBtnText}>7</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => this.handlePressNumber()}
+              style={styles.numberButton}
+            >
+              <Text style={styles.tablesBtnText}>8</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => this.handlePressNumber()}
+              style={styles.numberButton}
+            >
+              <Text style={styles.tablesBtnText}>9</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.buttonSection}>
+            <TouchableOpacity
+              onPress={() => this.handlePressNumber()}
+              style={styles.numberButton}
+            >
+              <MaterialCommunityIcons
+                name="backspace"
+                color="white"
+                size={30}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => this.handlePressNumber()}
+              style={styles.numberButton}
+            >
+              <Text style={styles.tablesBtnText}>0</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => this.handlePressNumber()}
+              style={styles.numberButton}
+            >
+              <MaterialCommunityIcons
+                name="keyboard-return"
+                color="white"
+                size={30}
+              />
+            </TouchableOpacity>
+          </View>
         </View>
 
       </View>
@@ -62,7 +161,7 @@ const styles = StyleSheet.create({
     flexGrow: 5,
     // backgroundColor: '#5BC2C1',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
 
   },
   bottomSection: {
@@ -70,11 +169,11 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     // backgroundColor: '#FD909E',
     // justifyContent: 'center',
-    // alignItems: 'left',
+    justifyContent: 'flex-end',
     
   },
   topText: {
-    fontSize: 40,
+    fontSize: 55,
     // color: '#586e75',
     // color: '#268bd2',
     color: '#df01c5',
@@ -85,9 +184,11 @@ const styles = StyleSheet.create({
     // backgroundColor: '#cde3f2',
     padding: 10,
     borderRadius: 3,
-    height: 50,
+    height: 70,
     margin: 4,
-    width: 85,
+    width: 115,
+    alignItems: 'center',
+    justifyContent: 'center',
 
   },
   numberButton2: {
@@ -103,7 +204,7 @@ const styles = StyleSheet.create({
   },
   tablesBtnText: {
     color: 'white',
-    fontSize: 22,
+    fontSize: 32,
     textAlign: 'center',
   },
   startButton: {
